479f668a77b39238bf910cc43d915569d08c75b1
Gtp87 <fabio.giannitrapani@gmail.com>
2022-02-15 15:53:22 +0100

    add ul

--- index.php
+++ index.php
@@ -49,7 +49,35 @@ class Movie
 
 $movie = new Movie('Avengers:Endgame', 'Fantascienza', '2019', '181m');
 $movie2 = new Movie('Black Panther', 'Fantascienza', '2018', '134m');
-var_dump($movie,$movie2);
+// var_dump($movie,$movie2);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movies</title>
+</head>
+<body>
+    <h1>Film</h1>
+<ul>
+    <li>Titolo: <?=$movie->getTitle()?></li>
+    <li>Genere: <?=$movie->getGenre()?></li>
+    <li>Anno: <?=$movie->getYear()?></li>
+    <li>Durata: <?=$movie->getDuration()?></li>
+</ul>
+
+<h1>Film</h1>
+<ul>
+    <li>Titolo: <?=$movie2->getTitle()?></li>
+    <li>Genere: <?=$movie2->getGenre()?></li>
+    <li>Anno: <?=$movie2->getYear()?></li>
+    <li>Durata: <?=$movie2->getDuration()?></li>
+</ul>  
+</body>
+</html>
 
 
 
